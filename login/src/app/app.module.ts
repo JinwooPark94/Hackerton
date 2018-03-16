06ec27945cc1e7937ba6077d952d21bd32a581d2
JinwooPark94 <wlsdntus4@gmail.com>
2018-03-16 10:19:32 +0900

    edit app folder

---
+++ login/src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+// Root Component
+import { AppComponent } from './app.component';
+
+// Common Component
+import { HeaderComponent } from './common/header.component';
+import { FooterComponent } from './common/footer.component';
+import { MainComponent } from './common/main.component';
+
+// Login Component
+import { LoginComponent } from './member/login/login.component';
+import { SignupComponent } from './member/signup/signup.component';
+
+// NotFound Component
+import { NotfoundComponent } from './notfound/notfound.component';
+
+const routes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: '**', component: NotfoundComponent}
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    SignupComponent,
+    MainComponent,
+    NotfoundComponent
+  ],
+  imports: [BrowserModule, RouterModule.forRoot(routes, { useHash: false })],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
